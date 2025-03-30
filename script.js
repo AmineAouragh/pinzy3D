63324e9907537455b0c3eb3b694faf6a089e4c73
AMINEAOURAGH <amine.aouragh.20@gmail.com>
2025-03-30 13:02:05 +0000

    improve 3D globe look

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 import * as THREE from 'three' 
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
+
 const w = window.innerWidth
 const h = window.innerHeight 
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000) 
-const renderer = new THREE.WebGLRenderer() 
+const renderer = new THREE.WebGLRenderer({ antialias: true }) 
 
 renderer.setSize(w, h) 
-document.body.append(renderer.domElement)
+document.body.appendChild(renderer.domElement)
 
-camera.position.z = 8
+renderer.toneMapping = THREE.ACESFilmicToneMapping 
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace
+
+const earthGroup = new THREE.Group() 
+scene.add(earthGroup)
+
+camera.position.z = 10
 
 const loader = new THREE.TextureLoader() 
 
-const geometry = new THREE.SphereGeometry(5, 128, 128) 
-const material = new THREE.MeshBasicMaterial({
-    map: loader.load('/textures/8081_earthmap4k.jpg')
+const geometry = new THREE.IcosahedronGeometry(4, 12) 
+const material = new THREE.MeshStandardMaterial({
+    map: loader.load('./textures/8081_earthmap4k.jpg')
 })
 
-const earth = new THREE.Mesh(geometry, material)
-scene.add(earth) 
+const earthMesh = new THREE.Mesh(geometry, material)
+earthGroup.add(earthMesh)
+
+const lightsMat = new THREE.MeshBasicMaterial({
+    // transparent: true,
+    // opacity: 0.6,
+    map: loader.load('./textures/8081_earthlights4k.jpg'),
+    blending: THREE.AdditiveBlending
+})
+const lightsMesh = new THREE.Mesh(geometry, lightsMat)
+
+earthGroup.add(lightsMesh)
 
 const sunLight = new THREE.DirectionalLight(0xffffff, 2.0)
 sunLight.position.set(-2, 0.5, 1.5)
@@ -31,7 +48,8 @@ new OrbitControls(camera, renderer.domElement)
 
 function animate(){
     requestAnimationFrame(animate)
-    earth.rotation.y += 0.001 
+    earthMesh.rotation.y += 0.002
+    lightsMesh.rotation.y += 0.002
     renderer.render(scene, camera)
 }
 
